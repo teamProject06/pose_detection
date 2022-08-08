353a0c6a247108fefe769dc27058e4f5d4229fdc
seungyyy <dus3214@gmail.com>
2022-08-08 23:22:46 +0900

    fix: 초기 세팅, 라우터 연결

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+
+import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
+import Main from './pages/Main';
+import RoutineCam from './pages/RoutineCam';
+import PoseCam from './pages/PoseCam';
+import Feedback from './pages/Feedback';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import MyPage from './pages/MyPage'
+import Routine from './pages/Routine'
+
 
 function App() {
   return (
-    <div >
-     
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Main />}></Route>
+          <Route path='/signin' element={<SignIn />}></Route>
+          <Route path='/signup' element={<SignUp />}></Route>
+          <Route path='/routine' element={<Routine />}>
+            <Route path='routinecam' element={<RoutineCam />}></Route>
+          </Route>
+          <Route path='/:id'>
+            <Route path='mypage' element={<MyPage />}></Route>
+          </Route>
+          <Route path='/posecam' element={<PoseCam />}>
+            <Route path='feedback' element={<Feedback />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
