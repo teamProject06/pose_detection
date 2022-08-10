fcd0b6b57d81c4cb94ff983e44e0d5b95959029a
yuulee <yourim0112@naver.com>
2022-08-10 12:33:31 +0900

    feat: Header.js 영상 추가

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 const MyPage = () => {
   return (
-    <div>MyPage</div>
+    <Container>
+      <Title>My Page</Title>
+      <Parent>
+        <Div>운동루틴</Div>
+        <Div>결과 및 피드백</Div>
+        <Div>시간(초) 및 횟수(회)</Div>
+      </Parent>
+    </Container>
   )
 }
 
-export default MyPage
+const Container = styled.div`
+  display : block;
+  text-align : center;
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 50px 10px;
+`;
+
+const Title = styled.div`
+  font-size : 30px;
+  text-size-adjust: none;
+  font-family: campton, "Apple SD Gothic Neo", NanumBarunGothic, 나눔바른고딕, "Malgun Gothic", "맑은 고딕", dotum, sans-serif;;
+  padding: 20% 0 0;
+  margin: 10%;
+`;
+
+const Parent = styled.div`
+display: flex;
+height: 200px;
+`
+
+const Div = styled.div`
+  font-family: campton, "Apple SD Gothic Neo", NanumBarunGothic, 나눔바른고딕, "Malgun Gothic", "맑은 고딕", dotum, sans-serif;;
+  width: 40%;
+  height : 45px;
+  color: #1d1d1d;
+  font-weight: 600;
+  font-size: 15px;
+  padding: 15px;
+  border-radius: 8px;
+  background-color : #c3dbff;
+  margin-left: 10px;
+  margin-right: 10px;
+`;
+
+
+export default MyPage;
