ca1f0af6653ae25937d631df365b02d33d0a725d
seungyyy <dus3214@gmail.com>
2022-08-11 11:06:39 +0900

    feat: 루틴 input 추가, 수정,삭제 기능

--- client/src/atom/atomState.js
+++ client/src/atom/atomState.js
@@ -1,48 +1,8 @@
 import { atom , selector } from 'recoil';
 
-let uniqId = 0
-
 export const routineListState = atom({
     key: 'routineListState',
     default: [], 
-})
-
-export const createRoutine = (name, count) => ({
-  id: ++uniqId,
-  done: false,
-  name,
-  count
-})
-
-export const routineState = atom({
-  key: 'routineState',
-  default: [
-     createRoutine('', ''),
-  ],
-})
-
-export const filterType = atom({
-  key: 'filterType',
-  default: 'all',
-})
-
-export const filteredRoutineState = selector({
-  key: 'filteredRoutineState',
-  get: ({ get }) => {
-    const items = get(routineState)
-    const type = get(filterType)
-
-    switch (type) {
-      case 'do':
-        return items.filter(routine => !routine.done)
-
-      case 'done':
-        return items.filter(routine => routine.done)
-
-      default:
-        return items
-    }
-  }
 })
 
 export const isRoutineCheckedState = atom({
@@ -50,9 +10,12 @@ export const isRoutineCheckedState = atom({
     default: {
         isAdd: true,
         routine: false,
-        edit: false,
-        editId: 0,
     }
+})
+
+export const editIdFilterState = atom({
+  key: 'editIdFilterState',
+  default: []
 })
 
 
--- client/src/components/routine/ToRoutin.js
+++ client/src/components/routine/ToRoutin.js
@@ -1,11 +1,20 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { useSetRecoilState, useRecoilValue, useRecoilState } from 'recoil';
+import { useRecoilValue, useRecoilSnapshot } from 'recoil';
 import {routineListState}  from '../../atom/atomState'
 import styled from 'styled-components';
 import RoutineItemCreate from './RoutineItemCreate';
+import RoutineItem from './RoutineItem';
 
 const ToRoutin = () => {
     const routineList = useRecoilValue(routineListState)
+
+    const snapshot = useRecoilSnapshot();
+    useEffect(() => {
+      console.debug('The following atoms were modified:');
+      for (const node of snapshot.getNodes_UNSTABLE({isModified: true})) {
+        console.debug(node.key, snapshot.getLoadable(node));
+      }
+    }, [snapshot]);
 
     useEffect(()=> {
         console.log(routineList)
@@ -13,15 +22,9 @@ const ToRoutin = () => {
   return (
     <>
     <RoutineItemCreate />
-       {routineList.map(it => {
-        return (
-            <>
-            {it.name} list
-            {it.count}
-            {it.isActive}
-            </>
-        )
-        })}
+       {routineList.map(item => (
+            <RoutineItem key={item.id} item={item} />
+        ))}
         {routineList.length > 0 && <button type='button'>완료</button>}
     </>
   )
