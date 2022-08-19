ade7804e1da6bbd5c9078e2f4e39d6e5dd7a71a1
lnalice <80760544+lnalice@users.noreply.github.com>
2022-08-19 09:48:51 +0900

    feat : add social login, login design

---
+++ client/src/components/user/socialLogin/NaverLoginCallback.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import port from "./../../../data/port.json";
+
+const NaverLoginCallback = () => {
+    
+    const navigate = useNavigate();
+    const [cookiesAuth, setCookieAuth, removeCookieAuth] = useCookies(["auth"]);
+    const [cookies, setCookie, removeCookie] = useCookies(["userInfo"]);
+    
+    const NAVER_CODE = new URL(window.location.href).searchParams.get("code");
+    useEffect(() => {
+        console.log("NAVER_CODE", NAVER_CODE);
+        sendCode().then((res) => {
+            console.log("res", res.data);
+            if (res.data.login) { //true면 로그인 되어있는상태
+                setCookie("userInfo", res.data, { path: "/" });
+                navigate("/");
+            } else {             //false면 회원가입을 진행해야하는 상태
+                setCookie("auth", res.data, { path: "/" });
+                navigate("/oauth/signUp");
+            }
+        }).catch(e => {
+            console.log(e);
+            // navigate("/");
+        })
+    },[]);
+    
+    const sendCode= async() =>{
+        return await axios.get(port.url + '/oauth/naver',{
+            params: {
+                code: NAVER_CODE
+            }
+        });}
+}
+
+export default NaverLoginCallback;
+
+
+/********************************************* */
+//         sendCode().then((res) => {
+//             console.log("res.data", res.data);
+
+//             if (res.data.login) { //true면 로그인 되어있는상태
+
+//                 setCookie("userData", res.data, { path: "/" });
+//                 navigate("/review/list");
+
+//             } else {             //false면 회원가입을 진행해야하는 상태
+//                 setCookie("auth", res.data, { path: "/" });
+//                 navigate("/oauth/signUp");
+//             }
+
+
+
+//         }).catch(e => {
+//             console.log(e);
+//             navigate("/");
+//         })
+
+//     }, []);
+
+//     const sendCode = async () => {
+//         return await axios.get(url.url + `/auth/kakao`, {
+//             params: {
+//                 code: KAKAO_PARAMS
+//             }
+//         });
+//     }
+
+// }
+// export default KakaoCallBack;
