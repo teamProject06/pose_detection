b54d7847fea8678e40e47105aa66c667e25bb98d
seungyyy <dus3214@gmail.com>
2022-08-29 14:34:21 +0900

    feat: 서버에서 루틴 정보 가져와서 달력 데이터에 맞게 변경,  style: 루틴 캘린더 스타일 수정

--- client/src/components/RoutineCalendar.js
+++ client/src/components/RoutineCalendar.js
@@ -1,73 +1,105 @@
 import React, { useEffect, useState, useRef, useCallback } from 'react'
 import Calendar from '@toast-ui/react-calendar';
 import '@toast-ui/calendar/dist/toastui-calendar.min.css';
-
 import 'tui-date-picker/dist/tui-date-picker.css';
 import 'tui-time-picker/dist/tui-time-picker.css';
 import styled from 'styled-components';
+import axios from 'axios';
+import port from "../data/port.json";
+import { useCookies } from "react-cookie";
+import { FaAngleLeft } from "react-icons/fa";
+import { FaAngleRight } from "react-icons/fa";
 
 const initialEvents = [
   {
     id: '1',
-    calendarId: 'cal1',
+    calendarId: 'mycalendar',
     title: 'Lunch',
     category: 'time',
     start: '2022-08-29T12:00:00',
     end: '2022-08-29T13:30:00',
   },
-  {
-    id: '2',
-    calendarId: 'cal1',
-    title: 'Coffee Break',
-    body: '스쿼트 4회, 럼비 4뢰',
-    state: null,
-    attendees: ['user'],
-    category: 'time',
-    start: '2022-08-28T15:00:00',
-    end: '2022-08-28T15:30:00',
-  },
-  {
-    id: '3',
-    calendarId: 'cal1',
-    title: 'test',
-    body: '런지 4회, 럼비 4뢰',
-    state: null,
-    attendees: ['user'],
-    category: 'time',
-    start: '2022-08-28T15:00:00',
-    end: '2022-08-28T15:30:00',
-    backgroundColor: 'red'
-  },
 ];
 
-const calendars = [{ id: 'cal1'}];
+const calendars = [{ id: 'mycalendar'}];
 
 const RoutineCalendar = () => {
   const calendarRef = useRef(null)
   const [selectedDateRangeText, setSelectedDateRangeText] = useState('');
+  const [cookies, setCookie, removeCookie] = useCookies(["userInfo"]);
+  const [calendarInitialList, setCalendarInitialList] = useState([])
   const getCalInstance = useCallback(() => calendarRef.current?.getInstance?.(), []);
+  const name = cookies.userInfo.name
+
+  const getRoutineData = async () => {
+    return await axios.get(`${port.url}/routine/${name}/mypage`)
+                  .then(res => {
+                    makeData(res.data, name)
+                  })
+                  .catch(err => console.log(err.message))
+  }
+
+  useEffect(() => {
+    getRoutineData()
+    
+  }, [])
+
+  const makeData = (data, name) => {
+    console.log(data.length)
+    for (let i = 0; i < data.length; i++) {
+      const timeToString = new Date(data[i].time).toISOString()
+      const routineTime = timeToString.split('.')
+        const myCalendarObj = {
+          id: `${name} ${Math.floor(Math.random() * 100000)}`,
+          calendarId: 'mycalendar',
+          title: '',
+          body: '',
+          state: null,
+          attendees: [name],
+          category: 'time',
+          start: routineTime[0],
+          end: routineTime[0],
+          backgroundColor: '#061673',
+        };
+  
+        let bodyText = '';
+        let routineLevel = 0;
+        for(let j = 0; j < data[i].routine.length; j++) {
+          bodyText += data[i].routine[j].name
+          if (data[i].routine[j].isActive) {
+            bodyText += `${data[i].routine[j].count}회, `
+          } else {
+            bodyText += `${data[i].routine[j].count}초, `
+          }
+          routineLevel ++
+          myCalendarObj['body'] = bodyText.slice(0, bodyText.length-2)
+        }
+        
+        if (routineLevel >=7) {
+          myCalendarObj['title'] = 'Hard Routine'
+        } else if (routineLevel > 3) {
+          myCalendarObj['title'] = 'Medium Routine'
+        } else {
+          myCalendarObj['title'] = 'Easy Routine'
+        }
+  
+        setCalendarInitialList([...calendarInitialList, myCalendarObj])
+        console.log(myCalendarObj, 'eeeeeeeeeeeeeeeeeeeeeeee')
+      }
+      console.log(calendarInitialList)
+  }
 
 
-  const onAfterRenderEvent = (event) => {
-    console.log(event.title);
-  };
+
 
   const updateRenderRangeText = () => {
-    const now = new Date()
-
-    let year = now.getFullYear();
-    let month = now.getMonth() + 1;
-    let date = now.getDate();
-    let dateRangeText;
-
-
-    const start = `${year}-${month < 10 ? '0' : ''}${month}-${date < 10 ? '0' : ''}${date}`;
-    const end = `${year}-${month < 10 ? '0' : ''}${month}-${
-      date < 10 ? '0' : ''
-        }${date}`;
-        dateRangeText = `${start} ~ ${end}`;
-        dateRangeText = `${year}-${month}-${date}`;
-
+    let dateRangeText = ''
+    const date = getCalInstance().getDate();
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    
+    dateRangeText = `${year}-${month}`;
+        
     setSelectedDateRangeText(dateRangeText);
   }
 
@@ -86,36 +118,33 @@ const RoutineCalendar = () => {
 
     return (
       <Container>
-          <span>
+          <MenuContainer onClick={onClickNavi} >
+            <span className='date-text'>{selectedDateRangeText}</span>
           <button
             type="button"
-            className="btn btn-default btn-sm move-day"
-            data-action="move-prev"
-            onClick={onClickNavi}
-          >
-            Prev
-          </button>
-          <button
-            type="button"
-            className="btn btn-default btn-sm move-today"
             data-action="move-today"
-            onClick={onClickNavi}
+            className='button today'
           >
             Today
           </button>
           <button
             type="button"
-            className="btn btn-default btn-sm move-day"
-            data-action="move-next"
-            onClick={onClickNavi}
+            data-action="move-prev"
+            className='button move'
           >
-            Next
+            <FaAngleLeft />
           </button>
-        </span>
-        <span className="render-range">{selectedDateRangeText}</span>
+          <button
+            type="button"
+            data-action="move-next"
+            className='button move'
+          >
+            <FaAngleRight />
+          </button>
+        </MenuContainer>
           <Calendar
           ref={calendarRef}
-        height="500px"
+        height="550px"
         view="month"
         month={{
           dayNames: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
@@ -124,17 +153,51 @@ const RoutineCalendar = () => {
         isReadOnly="true"
         useDetailPopup="true"
         calendars={calendars}
-        events={initialEvents}
-        onAfterRenderEvent={onAfterRenderEvent}
+        events={calendarInitialList}
       />
       </Container>
     );
   }
 
 const Container = styled.div`
+  position: relative;
+  width: 80%;
+  margin: 0 auto;
+  padding: 5em 2em 0;
   .toastui-calendar-detail-container .toastui-calendar-section-button {
     display: none;
   }
+  .toastui-calendar-template-time strong {
+    display: none;
+
+  }
 `;
+
+const MenuContainer = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  left: 2em;
+  top: 1em; 
+  .date-text {
+    font-size: 18px;
+    margin-right: 10px;
+  }
+  .button {
+    border: 1px solid #ddd;
+    z-index: 10;
+  }
+  .button.today {
+    border-radius: 15px;
+    padding: .7em 1em;
+    margin-right: 10px;
+  }
+  .button.move {
+    border-radius: 50%;
+    padding: .5em;
+    margin-right: 10px;
+  }
+  `;
+
 
 export default RoutineCalendar
